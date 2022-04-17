d12fb55ee78816534e29a3823d403663062cbee9
Ramash3828 <ramash3828@gmail.com>
2022-04-17 17:58:59 +0600

    blog page complete

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./About.css";
+import profilePic from "../../images/ramash.jpg";
 
 const About = () => {
     return (
-        <div>
-            <h2>About Page</h2>
+        <div className="container py-5">
+            <div className="row">
+                <div className="col-md-4 offset-4">
+                    <img
+                        className="img-fluid rounded-full"
+                        src={profilePic}
+                        alt=""
+                    />
+                </div>
+            </div>
         </div>
     );
 };
